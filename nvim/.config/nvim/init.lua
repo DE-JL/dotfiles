e029906c84a86a7117b9d5d65e69f7cf530fa4b5
DL <dejl@umich.edu>
2026-09-06 14:06:15 -0400

    Remove smartindent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,7 +12,6 @@ vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.smartindent = true
 
 -- Search
 vim.opt.ignorecase = true
